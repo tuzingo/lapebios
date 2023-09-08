c61b384d479367d208d4cf730f1e016e506a991b
Arthur Regueira <arthur.regueira@aol.com>
2023-09-08 13:27:17 -0300

    first

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Theme Name: Lapebios
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: The theme that's we use at https://iefe.ufal.br/laboratorio/lapebios/
+Version: 0.01
+Author: Arthur C Regueira de Miranda
+Author URI: http://github.com/tuzingo
+License: MIT
+*/
