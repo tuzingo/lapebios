07df74465dae3471dcf7a0066db9c3d62199f93e
Arthur Regueira <arthur.regueira@aol.com>
2023-09-08 16:40:21 -0300

    Lastofday

--- header.php
+++ header.php
@@ -1,1 +1,26 @@
-<?php
+<!DOCTYPE html >
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width" />
+    <?php if (is_search()) { ?>
+        <meta name="robots" content="noindex, nofollow" />
+    <?php } ?>
+    <title>
+        <?php 	wp_title( '|', true, 'right' ); bloginfo( 'name' );   ?>
+    </title>
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" />
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+    <header>
+        <nav class="navbar bg-body-tertiary sticky-top">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src=""" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+                    LAPEBIOS
+                </a>
+            </div>
+        </nav>
--- index.php
+++ index.php
@@ -1,11 +1,23 @@
-<?php
-
-/*
+<!--
 Theme Name: Lapebios
-Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Theme URI: http://URI_Of_Page_Describing_Plugin_and_Updates
 Description: The theme that's we use at https://iefe.ufal.br/laboratorio/lapebios/
 Version: 0.01
-Author: Arthur C Regueira de Miranda
+Author: Arthur Candido Regueira de Miranda
 Author URI: http://github.com/tuzingo
 License: MIT
 */
+-->
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <title><?php wp_title( '|', true, 'right' ); ?></title>
+    <link rel="stylesheet" href="<?php echo esc_url( get_stylesheet_uri() ); ?>" type="text/css" />
+    <?php wp_head(); ?>
+</head>
+<body>
+<?php get_header();?>
+<?php wp_footer(); ?>
+</body>
+</html>
